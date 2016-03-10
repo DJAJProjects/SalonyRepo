eb445fd2ec8e009a33b5dae006321a7c822d84aa
kubawieczorek <jwieczorek94@wp.pl>
2016-03-10 19:47:27 +0100

    Nowy projekt
    Dodany SpringBoot, Thymeleaf, Hibernate, konfiguracja połączenia z
    MySQL.

---
+++ SalonyProj/src/main/java/com/example/SalonyApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonyApplication.class, args);
+	}
+}
---
+++ SalonyProj/target/src/Entities/SamochodyEntity.java
@@ -0,0 +1,53 @@
+package Entities;
+
+import javax.persistence.*;
+
+/**
+ * Created by Kuba on 3/10/2016.
+ */
+@Entity
+@Table(name = "samochody", schema = "salonydb", catalog = "")
+public class SamochodyEntity {
+    private int id;
+    private String nazwa;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "nazwa")
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SamochodyEntity that = (SamochodyEntity) o;
+
+        if (id != that.id) return false;
+        if (nazwa != null ? !nazwa.equals(that.nazwa) : that.nazwa != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (nazwa != null ? nazwa.hashCode() : 0);
+        return result;
+    }
+}
