2a3893a007d287b3b8df0aab90039ababfb07990
Dominika94 <dominikablasiak2@gmail.com>
2016-04-09 14:33:32 +0200

    Kontroler webowy + bootstrap

---
+++ CarShowroomProj/src/main/java/pl/polsl/web/WorkerWebController.java
@@ -0,0 +1,26 @@
+package pl.polsl.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.polsl.controller.WorkersController;
+
+/**
+ * Created by Dominika Błasiak on 09.04.2016.
+ */
+@Controller
+public class WorkerWebController {
+    @Autowired
+    private WorkersController workersController;
+    /**
+     * Load on page about groups
+     * @param model
+     * @return worker.html
+     */
+    @RequestMapping(value ="/worker")
+    public String allGroups(Model model){
+        model.addAttribute("workers", workersController.findAll());
+        return "worker";
+    }
+}
