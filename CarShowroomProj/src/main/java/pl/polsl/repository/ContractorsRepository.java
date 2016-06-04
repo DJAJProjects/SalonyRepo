43c0fa9a8d3cd78831d3c35f58907a111a12b127
kubawieczorek <jwieczorek94@wp.pl>
2016-06-04 13:23:20 +0200

    filtrowanie gridow po uzytkowniku, wstęp
    filtrowanie gridow po uzytkowniku, wstęp

--- CarShowroomProj/src/main/java/pl/polsl/repository/ContractorsRepository.java
+++ CarShowroomProj/src/main/java/pl/polsl/repository/ContractorsRepository.java
@@ -5,19 +5,40 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import pl.polsl.model.Car;
-import pl.polsl.model.Contractor;
-import pl.polsl.model.Promotion;
-import pl.polsl.model.Showroom;
+import pl.polsl.model.*;
 
 import java.util.List;
 
 /**
- * Created by Aleksandra on 2016-04-15.
+ * Created by Kuba on 2016-06-02.
  */
 @Repository
 @Transactional
 public interface ContractorsRepository extends PagingAndSortingRepository<Contractor, Integer>{
+    @Query(value = "select contractor from Contractor contractor " +
+                         "where exists (select contract from Contract contract  " +
+                            " where contract.contractor = contractor " +
+                            " and contract.worker.showroom.director = director )")
+    public List<Contractor> findRelatedToDirector ( @Param("director")Worker director);
 
+
+    /**
+     * @return List of clients related to salesman by contract model.
+     */
+    @Query(value = "select contractor from Contractor contractor " +
+            "where exists (select contract from Contract contract  " +
+                "where contract.contractor = contractor" +
+                "and contract.worker = :salesman) ")
+    public List<Contractor> findRelatedToSalesman( @Param("salesman")Worker salesman);
+
+    /**
+     * @return List of clients related to serviceman by car model:
+     * Model of car servised by serviceman and related by contract model with client.
+     */
+    @Query(value = "select contractor from Contractor contractor " +
+            "where exists (select service from Service service  " +
+            "where service.serviceman = :serviceman" +
+            "and contract.worker = :salesman) ")
+    public List<Contractor> findRelatedToServiceman( @Param("serviceman")Worker serviceman);
 }
 
